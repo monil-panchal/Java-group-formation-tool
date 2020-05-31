0e7d571131fe6cb34c8988087e6f30325b98f403
darshan <darshan.kathiriya@dal.ca>
2020-05-31 12:39:34 -0300

    Add Emrollment and Email

--- src/test/java/com/assessme/service/EnrollmentServiceTest.java
+++ src/test/java/com/assessme/service/EnrollmentServiceTest.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
