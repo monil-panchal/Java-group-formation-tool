5bc437feaab578866fb31c7bb5ac09957a1a8fbf
HARDIK PATEL <hr325484@dal.ca>
2020-06-01 11:43:12 -0300

    Course Module added

---
+++ src/main/java/com/assessme/db/dao/CourseDAOImpl.java
@@ -0,0 +1,138 @@
+package com.assessme.db.dao;
+
+import com.assessme.db.connection.DBConnectionBuilder;
+import com.assessme.model.Course;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author: hardik
+ * Created on: 2020-05-30
+ */
+
+/**
+ * The {@link CourseDAO} implementation class for performing CRUD operations for the course table of the database.
+ */
+@Repository
+public class CourseDAOImpl implements CourseDAO {
+
+    private Logger logger = LoggerFactory.getLogger(CourseDAOImpl.class);
+
+    @Autowired
+    private DBConnectionBuilder dbConnectionBuilder;
+
+    private Optional<Connection> connection;
+
+    // CourseDAO method for retrieving course by using courseCode
+    @Override
+    public Optional<Course> getCourseByCode(String courseCode) throws Exception {
+
+        Optional<Course> course = Optional.empty();
+
+        // SQL query for fetching the course record based on the courseCode
+        String selectQuery = "SELECT * FROM course WHERE course_code =" +  courseCode;
+
+		try {
+            // Getting the DB connection
+            connection = dbConnectionBuilder.createDBConnection();
+
+            // Preparing the statement
+            Statement statement = connection.get().createStatement();
+
+            if ((!courseCode.isEmpty() && courseCode != null)) {
+
+                ResultSet resultSet = statement.executeQuery(selectQuery);
+
+                if (!resultSet.isBeforeFirst()) {
+                    logger.error(String.format("Course: %s is not found in the database", courseCode));
+                    throw new Exception(String.format("Course: %s is not found in the database", courseCode));
+                }
+
+                logger.info(String.format("Course data retrieved successfully"));
+                while (resultSet.next()) {
+
+                    course = Optional.of(new Course());	
+                    course.get().setCourseId(resultSet.getInt("course_code"));
+                    course.get().setCourseName(resultSet.getString("course_name"));
+
+                }
+                String successString = String.format("Course with course code: %s retrieved successfully.", courseCode);
+                logger.info(successString);
+
+            } else
+                throw new Exception(String.format("Course: %s record is not found in the Database.", courseCode));
+
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return course;
+    }
+
+
+    @Override
+    public List<Course> getAllCourse() throws SQLException, ClassNotFoundException {
+
+        // SQL query for fetching all the courses
+        String selectCourseQuery = "SELECT * FROM course";
+
+        List<Course> courseList = new ArrayList<>();
+
+        try {
+            // Getting the DB connection
+            connection = dbConnectionBuilder.createDBConnection();
+
+            // Preparing the statement
+            PreparedStatement preparedStatement = connection.get().prepareStatement("SELECT * FROM course");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+
+                // Instantiating new course
+            	Course course = new Course();
+
+                //Setting the attributes
+                course.setCourseId(resultSet.getInt("course_code"));
+                course.setCourseName(resultSet.getString("course_name"));
+
+                // Adding course to the list
+                courseList.add(course);
+            }
+
+            //Closing the connection
+            dbConnectionBuilder.closeConnection(connection.get());
+
+            logger.info(String.format("Course list retrieved from the database: %s", courseList));
+
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage());
+            e.printStackTrace();
+            throw e;
+        }
+        return courseList;
+    }
+
+    @Override
+    public Boolean addCourse(Course course) throws Exception {
+        return null;
+    }
+    
+    @Override
+    public Boolean removeCourse(Course course) throws Exception {
+        return null;
+    }
+    /*
+    @Override
+    public Optional<Course> updateCourse(Course course) throws Exception {
+        return Optional.empty();
+    }*/
+}
---
+++ src/test/java/com/assessme/service/CourseServiceTest.java
@@ -0,0 +1,59 @@
+package com.assessme.service;
+
+import com.assessme.db.dao.CourseDAOImpl;
+import com.assessme.model.Course;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author: hardik
+ * Created on: 2020-05-30
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class CourseServiceTest {
+
+    private Logger logger = LoggerFactory.getLogger(CourseServiceTest.class);
+
+    @Mock
+    private CourseDAOImpl courseDAO;
+
+    @MockBean
+    private Course course;
+
+    // Unit test
+    @Test
+    public void getCourseWithCodeTest() throws Exception {
+
+        logger.info("Running unit test for fetching course with course code");
+
+        String courseCode = "CSCI5308";
+        
+        course.setCourseName("Advance Software Development Concepts");
+        
+        Mockito.when(courseDAO.getCourseByCode(courseCode)).thenReturn(Optional.ofNullable(course));
+
+    }
+
+    //Unit test
+    @Test
+    public void getCourseListTest() throws Exception {
+
+        logger.info("Running unit test for fetching all courses");
+
+        List<Course> courseList = List.of(course);
+        Mockito.when(courseDAO.getAllCourse()).thenReturn(courseList);
+        Assert.notEmpty(courseDAO.getAllCourse(), "course list is not null");
+
+    }
+
+}
