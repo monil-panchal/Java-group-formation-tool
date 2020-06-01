aca9668042b8cc3185855a11bee5b0895344e70c
darshan <darshan.kathiriya@dal.ca>
2020-06-01 17:43:21 -0300

    add changes from develop branch

---
+++ src/main/java/com/assessme/service/MailSenderService.java
@@ -0,0 +1,52 @@
+package com.assessme.service;
+
+import com.assessme.SystemConfig;
+import com.assessme.config.EmailConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.Async;
+
+/**
+ * @author Darshan Kathiriya
+ * @created 31-May-2020 11:33 AM
+ */
+public class MailSenderService {
+
+    private Logger logger = LoggerFactory.getLogger(MailSenderService.class);
+
+    private static MailSenderService instance;
+    private JavaMailSenderImpl mailSender;
+
+    public MailSenderService(){
+        mailSender = getConfigured();
+    }
+
+    public static MailSenderService getInstance() {
+        if (instance == null) {
+            instance = new MailSenderService();
+        }
+        return instance;
+    }
+
+    private JavaMailSenderImpl getConfigured() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        EmailConfig emailConfig = SystemConfig.getInstance().getEmailConfig();
+        javaMailSender.setHost(emailConfig.getHost());
+        javaMailSender.setPort(emailConfig.getPort());
+        javaMailSender.setUsername(emailConfig.getUsername());
+        javaMailSender.setPassword(emailConfig.getPassword());
+        javaMailSender.setJavaMailProperties(emailConfig.getProps());
+        return javaMailSender;
+    }
+
+    @Async
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
