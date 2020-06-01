8044e48419618936785d81209248f51dd52f8381
darshan <darshan.kathiriya@dal.ca>
2020-06-01 17:43:21 -0300

    add changes from develop branch

--- src/main/java/com/assessme/controller/CSVUploadController.java
+++ src/main/java/com/assessme/controller/CSVUploadController.java
@@ -9,10 +9,21 @@ import com.assessme.model.User;
 import com.assessme.service.CSVImport;
 import com.assessme.service.CSVStorageService;
 import com.assessme.service.UserServiceImpl;
+import com.assessme.db.dao.EnrollmentDAO;
+import com.assessme.db.dao.EnrollmentDAOImpl;
+import com.assessme.model.Enrollment;
+import com.assessme.model.ResponseDTO;
+import com.assessme.model.Role;
+import com.assessme.model.User;
+import com.assessme.service.*;
+import com.assessme.util.AppConstant;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,15 +41,22 @@ import java.util.Optional;
 public class CSVUploadController {
 
     private Logger logger = LoggerFactory.getLogger(CSVUploadController.class);
-    // private StorageService service = CSVStorageService.getInstance();
-    //private UserServiceImpl userService = UserServiceImpl.getInstance();
-    private UserServiceImpl userServiceImpl;
+    private UserServiceImpl userService;
     private CSVStorageService service;
 
     public CSVUploadController(CSVStorageService service, UserServiceImpl userServiceImpl) {
         this.service = service;
-        this.userServiceImpl = userServiceImpl;
+        this.userService = userServiceImpl;
     }
+
+
+    private EnrollmentDAO enrollmentDAO = EnrollmentDAOImpl.getInstance();
+
+    private MailSenderService mailSenderService = MailSenderService.getInstance();
+
+    @Autowired
+    private RoleService roleService;
+
 
     @GetMapping("/csvupload")
     public String csvFileUploadForm(Model model) {
@@ -54,26 +72,31 @@ public class CSVUploadController {
             CSVReader reader = CSVImport.importFromPath(service.load(newFileName));
             logger.info("CSVParsed Successfully");
             List<String[]> allStudentsList = reader.readAll();
-            List<String> successfullInsertion = new ArrayList<>();
-            List<String> failedInsertion = new ArrayList<>();
-
-            for (String[] userRow : allStudentsList) {
-                logger.info(String.format("UserEmail: %s", userRow[3]));
-                Optional<User> userWithEmail = userServiceImpl.getUserFromEmail(userRow[3]);
+            Optional<Role> studentRole = roleService.getRoleFromRoleName("STUDENT");
+            for (String[] csvRow : allStudentsList) {
+                logger.info(String.format("UserEmail: %s", csvRow[3]));
+                Optional<User> userWithEmail = userService.getUserFromEmail(csvRow[3]);
+                long userId;
                 if (userWithEmail.isPresent()) {
                     // update uer role for that student.
-                    long userId = userWithEmail.get().getUserId();
+                    userId = userWithEmail.get().getUserId();
 //                    userService.updateUserRole(userId, "UserRole");
                 } else {
-                    User user = new User(userRow[0], userRow[1], userRow[2], userRow[3], "password", true);
-//                    userService.insertUser();
-                    Optional<User> insertedUser = userServiceImpl.getUserFromEmail(userRow[3]);
-                    if (insertedUser.isPresent()) {
-                        successfullInsertion.add(insertedUser.get().getBannerId());
-                    } else {
-                        failedInsertion.add(userRow[1]);
-                    }
+
+                    User newUser = new User();
+                    newUser.setBannerId(csvRow[0]);
+                    newUser.setLastName(csvRow[1]);
+                    newUser.setFirstName(csvRow[2]);
+                    newUser.setEmail(csvRow[3]);
+                    Optional<User> insertedUser = userService.addUser(newUser, AppConstant.DEFAULT_USER_ROLE_CREATE);
+                    userId = insertedUser.get().getUserId();
+                    mailSenderService.sendSimpleMessage(insertedUser.get().getEmail(),
+                            "Your Account Has Been Created",
+                            "Your password is YourBannerId_YourLastName");
                 }
+                Enrollment enrollment = new Enrollment(userId, studentRole.get().getRoleId(), 1L);
+                enrollmentDAO.insertEnrollment(enrollment);
+
             }
 
             redirectAttributes.addFlashAttribute("message",
