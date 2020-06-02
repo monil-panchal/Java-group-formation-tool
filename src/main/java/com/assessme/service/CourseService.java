60d35f7bd444e8f0652ad2398ffd1e4e3a997dad
HARDIK PATEL <hr325484@dal.ca>
2020-06-02 19:34:24 -0300

    add the missing method getCourseByName in CourseService

--- src/main/java/com/assessme/service/CourseService.java
+++ src/main/java/com/assessme/service/CourseService.java
@@ -69,4 +69,23 @@ public class CourseService {
         }
         return course;
     }
+    
+    /**
+     * Service method for retrieving course using courseName
+     */
+    public Optional<Course> getCourseWithName(String courseName) throws Exception {
+
+        Optional<Course> course;
+        try {
+            course = courseDAOImpl.getCourseByName(courseName);
+            String resMessage = String.format("Course with course name: %s has been retrieved from the database", courseName);
+            logger.info(resMessage);
+        } catch (Exception e) {
+            String errMessage = String.format("Error in retrieving the course from the database");
+            logger.error(errMessage);
+            e.printStackTrace();
+            throw e;
+        }
+        return course;
+    }
 }
