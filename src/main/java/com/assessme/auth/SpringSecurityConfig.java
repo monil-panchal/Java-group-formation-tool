48f57b46b67bec855a58bd25501a67f20d30da7b
Monil Panchal <mn204627@dal.ca>
2020-06-02 23:42:43 -0300

    [ADDING] user role update module

--- src/main/java/com/assessme/auth/SpringSecurityConfig.java
+++ src/main/java/com/assessme/auth/SpringSecurityConfig.java
@@ -26,10 +26,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers(
                         "/user/**",
+                        "/user",
+                        "/user/roles/**",
                         "/js/**",
                         "/css/**",
                         "/img/**",
