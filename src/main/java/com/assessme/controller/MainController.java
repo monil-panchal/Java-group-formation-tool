85aac9e409fd920c96ad9859c6e0750ad44d2912
baheti <rs997774@dal.ca>
2020-06-04 15:12:37 -0300

    integrated add course functionality for admin

--- src/main/java/com/assessme/controller/MainController.java
+++ src/main/java/com/assessme/controller/MainController.java
@@ -1,10 +1,15 @@
 package com.assessme.controller;
 
+import com.assessme.db.connection.DBConnectionBuilder;
+import com.assessme.db.dao.CourseDAOImpl;
+import com.assessme.model.Course;
 import com.assessme.model.User;
+import com.assessme.service.CourseService;
 import com.assessme.service.UserServiceImpl;
 import com.assessme.util.AppConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +33,11 @@ public class MainController {
 
 
     private UserServiceImpl userServiceImpl;
+    private CourseService courseService;
 
-    public MainController(UserServiceImpl userServiceImpl){
+    public MainController(UserServiceImpl userServiceImpl, CourseService courseService){
         this.userServiceImpl = userServiceImpl;
+        this.courseService = courseService;
     }
     
     @GetMapping("/login")
@@ -66,6 +73,30 @@ public class MainController {
 
     @GetMapping("/course_admin")
     public String courseAdmin(Model model) {
+        Course course = new Course();
+        model.addAttribute("course", course);
+        return "course_admin";
+    }
+
+    @PostMapping("/course_admin")
+    public String addCourse(@ModelAttribute("course") Course course) throws Exception {
+        logger.info("in add course");
+        Optional<Course> courseToAdd = Optional.empty();
+//        CourseService courseService = new CourseService();
+//        CourseDAOImpl courseDAO = new CourseDAOImpl(new DBConnectionBuilder());
+        try {
+//            registered = userServiceImpl.addUser(user, AppConstant.DEFAULT_USER_ROLE_CREATE);
+            logger.info("course code:");
+            logger.info(course.getCourseCode());
+            logger.info("course name:");
+            logger.info(course.getCourseName());
+//            courseToAdd = courseDAO.addCourse(course);
+            courseToAdd = courseService.addCourse(course);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+            throw e;
+        }
         return "course_admin";
     }
 
