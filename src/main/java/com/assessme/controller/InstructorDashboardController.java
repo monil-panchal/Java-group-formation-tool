2b09e7777e258b13d316c0ad8866b21f924bffa7
darshan <darshan.kathiriya@dal.ca>
2020-06-10 22:18:32 -0300

    Chages in Test for InstructorDashboardController

--- src/main/java/com/assessme/controller/InstructorDashboardController.java
+++ src/main/java/com/assessme/controller/InstructorDashboardController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.Connection;
@@ -22,7 +23,7 @@ import java.util.Optional;
  * @author Darshan Kathiriya
  * @created 04-June-2020 3:48 PM
  */
-@Controller
+@RestController
 public class InstructorDashboardController {
     private Logger logger = LoggerFactory.getLogger(InstructorDashboardController.class);
 
@@ -34,26 +35,22 @@ public class InstructorDashboardController {
         this.roleService = roleService;
     }
 
-    @GetMapping("/instructor_dashboard")
-    public String getDashboardAfterProcessing(Model model){
-        return "instructor_dashboard";
-    }
-
     @PostMapping("/instructor_dashboard")
-    public String getDashboard(@RequestParam long userId, Model model){
+    public ModelAndView getDashboard(@RequestParam long userId){
+        ModelAndView mav = new ModelAndView("instructor_dashboard");
         logger.info(String.format("Getting Dashboard for Instructor %d", userId));
         try{
             int roleIdIns = roleService.getRoleFromRoleName("INSTRUCTOR").get().getRoleId();
             int roleIdTa = roleService.getRoleFromRoleName("TA").get().getRoleId();
             List<Course> courseList = courseService.getCoursesByUserAndRole(userId, roleIdTa).get();
             courseList.addAll(courseService.getCoursesByUserAndRole(userId, roleIdIns).get());
-            model.addAttribute("courses", courseList);
-            model.addAttribute("userId", userId);
+            mav.addObject("courses", courseList);
+            mav.addObject("userId", userId);
         }catch(Exception e){
             logger.error("Error Getting Courses");
-            model.addAttribute("message", "Error Fetching Courses");
+            mav.addObject("message", "Error Fetching Courses");
         }
-        return "instructor_dashboard";
+        return mav;
     }
 
 }
--- src/test/java/com/assessme/controller/InstructorDashboardControllerTest.java
+++ src/test/java/com/assessme/controller/InstructorDashboardControllerTest.java
@@ -15,9 +15,11 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,14 +31,14 @@ import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author Darshan Kathiriya
  * @created 04-June-2020 2:54 PM
  */
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+//@ExtendWith(MockitoExtension.class)
 class InstructorDashboardControllerTest {
     private Logger logger = LoggerFactory.getLogger(InstructorDashboardControllerTest.class);
     private MockMvc mockMvc;
@@ -51,10 +53,14 @@ class InstructorDashboardControllerTest {
     InstructorDashboardController controller;
 
     @BeforeEach
-    void init(){
+    void init() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("templates/");
+        viewResolver.setSuffix(".html");
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders
                 .standaloneSetup(controller)
+                .setViewResolvers(viewResolver)
                 .addFilters()
                 .build();
     }
@@ -63,12 +69,22 @@ class InstructorDashboardControllerTest {
     void getDashboard() throws Exception {
         when(roleService.getRoleFromRoleName("INSTRUCTOR"))
                 .thenReturn(Optional.of(new Role(1, "INSTRUCTOR")));
+        when(roleService.getRoleFromRoleName("TA"))
+                .thenReturn(Optional.of(new Role(1, "INSTRUCTOR")));
         List<Course> courseList = new ArrayList<>();
-        courseList.add(new Course("CSCI_TEST","TEST Course"));
+        courseList.add(new Course("CSCI_TEST", "TEST Course"));
         assertTrue(roleService.getRoleFromRoleName("INSTRUCTOR").isPresent());
         when(courseService.getCoursesByUserAndRole(1L, 1))
                 .thenReturn(Optional.of(courseList));
         assertTrue(courseService.getCoursesByUserAndRole(1L, 1).isPresent());
-
+        mockMvc.perform(post("/instructor_dashboard")
+                .param("userId", "1")
+        )
+//                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("courses"))
+                .andExpect(model().attributeExists("userId"))
+                .andExpect(model().attributeDoesNotExist("message"))
+        ;
     }
 }
