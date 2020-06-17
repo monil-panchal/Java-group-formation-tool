785403d5804a3bc724595b1c125f756ccecd88be
baheti <rs997774@dal.ca>
2020-06-17 12:46:16 -0300

    added class

---
+++ src/main/java/com/assessme/db/CallStoredProcedure.java
@@ -0,0 +1,72 @@
+package com.assessme.db;
+
+import com.assessme.db.connection.DBConnectionBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.Optional;
+
+public class CallStoredProcedure {
+
+    private Logger logger = LoggerFactory.getLogger(DBConnectionBuilder.class);
+
+    private DBConnectionBuilder dbConnectionBuilder;
+    private String procedureName;
+    private Optional<Connection> connection;
+    private CallableStatement callableStatement;
+
+    public CallStoredProcedure(DBConnectionBuilder dbConnectionBuilder, String procedureName) throws SQLException, ClassNotFoundException{
+        this.procedureName = procedureName;
+        this.dbConnectionBuilder = dbConnectionBuilder;
+        connection = dbConnectionBuilder.createDBConnection();
+        callableStatement = connection.get().prepareCall("{call " +  procedureName + "}");
+    }
+
+    public void setParameter(int index, String value) throws SQLException{
+        callableStatement.setString(index, value);
+    }
+
+    public void setParameter(int index, Long value) throws SQLException{
+        callableStatement.setLong(index, value);
+    }
+
+    public void setParameter(int index, int value) throws SQLException{
+        callableStatement.setInt(index, value);
+    }
+
+    public ResultSet getResultSet() throws SQLException{
+        if(callableStatement.execute()){
+            return callableStatement.getResultSet();
+        }
+        else{
+            return null;
+        }
+    }
+
+    public void execute() throws SQLException
+    {
+        callableStatement.execute();
+    }
+
+    public int executeUpdate() throws SQLException
+    {
+         return callableStatement.executeUpdate();
+    }
+
+    public void finalSteps(){
+        try{
+            if(callableStatement != null){
+                callableStatement.close();
+            }
+
+            if(connection != null){
+                if(connection.get().isClosed()){
+                    connection.get().close();
+                }
+            }
+        }catch (Exception e){
+            logger.error(e.getMessage());
+        }
+    }
+}
