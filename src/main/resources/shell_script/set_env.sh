d7683bd3ba1f58dfc244a63f5d6c4d881372b714
Darshan <darshan.kathiriya@dal.ca>
2020-06-18 09:15:32 -0300

    Add variables for all platform

--- src/main/resources/shell_script/set_env.sh
+++ src/main/resources/shell_script/set_env.sh
@@ -1,7 +1,26 @@
-export DB_URL="db-5308.cs.dal.ca"
-export DB_NAME="CSCI5308_13_DEVINT"
-export DB_USER="CSCI5308_13_DEVINT_USER"
-export DB_PASSWORD="CSCI5308_13_DEVINT_13329"
-export EMAIL_HOST="smtp.mailtrap.io"
-export EMAIL_USERNAME="6612d1ce0bdec9"
-export EMAIL_PASSWORD="432f71435e7ae5"
+## Dev Server
+#export DB_URL="db-5308.cs.dal.ca"
+#export DB_NAME="CSCI5308_13_DEVINT"
+#export DB_USER="CSCI5308_13_DEVINT_USER"
+#export DB_PASSWORD="CSCI5308_13_DEVINT_13329"
+#export EMAIL_HOST="smtp.mailtrap.io"
+#export EMAIL_USERNAME="6612d1ce0bdec9"
+#export EMAIL_PASSWORD="432f71435e7ae5"
+
+## Test Server
+#export DB_URL="db-5308.cs.dal.ca"
+#export DB_NAME="CSCI5308_13_TEST"
+#export DB_USER="CSCI5308_13_TEST_USER"
+#export DB_PASSWORD="CSCI5308_13_TEST_13417"
+#export EMAIL_HOST="smtp.mailtrap.io"
+#export EMAIL_USERNAME="6612d1ce0bdec9"
+#export EMAIL_PASSWORD="432f71435e7ae5"
+
+## Prod Server
+#export DB_URL="db-5308.cs.dal.ca"
+#export DB_NAME="CSCI5308_13_PRODUCTION"
+#export DB_USER="CSCI5308_13_PRODUCTION_USER"
+#export DB_PASSWORD="CSCI5308_13_PRODUCTION_13807"
+#export EMAIL_HOST="smtp.mailtrap.io"
+#export EMAIL_USERNAME="6612d1ce0bdec9"
+#export EMAIL_PASSWORD="432f71435e7ae5"
