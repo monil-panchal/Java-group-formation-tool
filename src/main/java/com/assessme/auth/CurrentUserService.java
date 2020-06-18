99c6fd5c6b0614179e0d2c7afe378872320a4ae3
Darshan Kathiriya <dr590220@dal.ca>
2020-06-18 01:21:09 -0300

    Feature environment variables

--- src/main/java/com/assessme/auth/CurrentUserService.java
+++ src/main/java/com/assessme/auth/CurrentUserService.java
@@ -3,8 +3,12 @@ package com.assessme.auth;
 import com.assessme.model.User;
 import com.assessme.service.UserService;
 import com.assessme.service.UserServiceImpl;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CurrentUserService {
+  private Logger logger = LoggerFactory.getLogger(CurrentUserService.class);
   private UserService userService;
 
   public CurrentUserService(UserService userService) {
@@ -39,8 +44,10 @@ public class CurrentUserService {
 
   public boolean isInstructor() throws Exception {
     User user = getAuthenticatedUser().get();
+
     Set<String> userRoles = userService.getUserWithRolesFromEmail(user.getEmail()).get()
         .getUserRoles();
+    logger.info(Arrays.toString(userRoles.toArray()));
     return userRoles.contains("INSTRUCTOR");
   }
 }
