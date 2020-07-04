4d5c84b40e73846bc2bdf1aae89413b71aaa1988
baheti <rs997774@dal.ca>
2020-07-04 11:25:29 -0300

    added model tests

---
+++ src/test/java/com/assessme/model/UserRoleDTOTest.java
@@ -0,0 +1,44 @@
+package com.assessme.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UserRoleDTOTest {
+
+    @Test
+    public void setUserRolesTest(){
+        Set<String> roles = new HashSet<>(
+                List.of("ADMIN",
+                        "INSTRUCTOR",
+                        "STUDENT",
+                        "TA",
+                        "GUEST"));
+        UserRoleDTO userRoleDTO = new UserRoleDTO();
+        Assertions.assertNull(userRoleDTO.getUserRoles());
+
+        userRoleDTO.setUserRoles(roles);
+        Assertions.assertNotNull(userRoleDTO.getUserRoles());
+        Assertions.assertEquals(userRoleDTO.getUserRoles(), roles);
+    }
+
+    @Test
+    public void getUserRolesTest(){
+        Set<String> roles = new HashSet<>(
+                List.of("ADMIN",
+                        "INSTRUCTOR",
+                        "STUDENT",
+                        "TA",
+                        "GUEST"));
+        UserRoleDTO userRoleDTO = new UserRoleDTO();
+        Assertions.assertNull(userRoleDTO.getUserRoles());
+
+        userRoleDTO.setUserRoles(roles);
+        Assertions.assertNotNull(userRoleDTO.getUserRoles());
+        Assertions.assertEquals(userRoleDTO.getUserRoles(), roles);
+    }
+}
