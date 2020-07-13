97ea849293ec75ac9f5576a7122aa587dd7368b4
HARDIK PATEL <hr325484@dal.ca>
2020-07-13 17:02:11 -0300

    SurveyTests added

--- src/main/java/com/assessme/model/Survey.java
+++ src/main/java/com/assessme/model/Survey.java
@@ -3,50 +3,50 @@ package com.assessme.model;
 import java.util.List;
 
 public class Survey {
-    private long surveyId;
-    private long courseId;
-    private String surveyName;
-    private boolean surveyState;
+  private long surveyId;
+  private long courseId;
+  private String surveyName;
+  private boolean surveyState;
 
-    public Survey(){
-    }
+  public Survey(){
+  }
 
-    public Survey(long surveyId, long courseId, String surveyName, boolean surveyState){
-        this.surveyId = surveyId;
-        this.courseId = courseId;
-        this.surveyName = surveyName;
-        this.surveyState = surveyState;
-    }
+  public Survey(long surveyId, long courseId, String surveyName, boolean surveyState){
+    this.surveyId = surveyId;
+    this.courseId = courseId;
+    this.surveyName = surveyName;
+    this.surveyState = surveyState;
+  }
 
-    public long getSurveyId() {
-        return surveyId;
-    }
+  public long getSurveyId() {
+    return surveyId;
+  }
 
-    public void setSurveyId(long surveyId) {
-        this.surveyId = surveyId;
-    }
+  public void setSurveyId(long surveyId) {
+    this.surveyId = surveyId;
+  }
 
-    public long getCourseId() {
-        return courseId;
-    }
+  public long getCourseId() {
+    return courseId;
+  }
 
-    public void setCourseId(long courseId) {
-        this.courseId = courseId;
-    }
+  public void setCourseId(long courseId) {
+    this.courseId = courseId;
+  }
 
-    public String getSurveyName() {
-        return surveyName;
-    }
+  public String getSurveyName() {
+    return surveyName;
+  }
 
-    public void setSurveyName(String surveyName) {
-        this.surveyName = surveyName;
-    }
+  public void setSurveyName(String surveyName) {
+    this.surveyName = surveyName;
+  }
 
-    public boolean isSurveyState() {
-        return surveyState;
-    }
+  public boolean getSurveyState() {
+    return surveyState;
+  }
 
-    public void setSurveyState(boolean surveyState) {
-        this.surveyState = surveyState;
-    }
+  public void setSurveyState(boolean surveyState) {
+    this.surveyState = surveyState;
+  }
 }
--- src/main/java/com/assessme/model/SurveyQuestions.java
+++ src/main/java/com/assessme/model/SurveyQuestions.java
@@ -1,30 +1,29 @@
 package com.assessme.model;
 
 public class SurveyQuestions {
-    private long surveyId;
-//    private long courseId;
-    private long questionId;
+  private long surveyId;
+  private long questionId;
 
-    public SurveyQuestions(){}
+  public SurveyQuestions(){}
 
-    public SurveyQuestions(long surveyId, long questionId){
-        this.surveyId = surveyId;
-        this.questionId = questionId;
-    }
+  public SurveyQuestions(long surveyId, long questionId){
+    this.surveyId = surveyId;
+    this.questionId = questionId;
+  }
 
-    public long getSurveyId() {
-        return surveyId;
-    }
+  public long getSurveyId() {
+    return surveyId;
+  }
 
-    public void setSurveyId(long surveyId) {
-        this.surveyId = surveyId;
-    }
+  public void setSurveyId(long surveyId) {
+    this.surveyId = surveyId;
+  }
 
-    public long getQuestionId() {
-        return questionId;
-    }
+  public long getQuestionId() {
+    return questionId;
+  }
 
-    public void setQuestionId(long questionId) {
-        this.questionId = questionId;
-    }
+  public void setQuestionId(long questionId) {
+    this.questionId = questionId;
+  }
 }
