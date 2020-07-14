99a7141fd820f3efd139a7ca81fce4935948941d
Monil Panchal <mn204627@dal.ca>
2020-07-14 13:13:52 -0300

    [Add] Models related to Survey module

--- src/main/java/com/assessme/model/Survey.java
+++ src/main/java/com/assessme/model/Survey.java
@@ -1,51 +1,97 @@
 package com.assessme.model;
 
+import java.sql.Timestamp;
 
 public class Survey {
-  private long surveyId;
-  private long courseId;
-  private String surveyName;
-  private boolean surveyState;
 
-  public Survey(){
-  }
+    private long surveyId;
+    private String surveyName;
+    private String description;
+    private String state;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private long userId;
 
-  public Survey(long surveyId, long courseId, String surveyName, boolean surveyState){
-    this.surveyId = surveyId;
-    this.courseId = courseId;
-    this.surveyName = surveyName;
-    this.surveyState = surveyState;
-  }
 
-  public long getSurveyId() {
-    return surveyId;
-  }
+    public Survey() {
+    }
 
-  public void setSurveyId(long surveyId) {
-    this.surveyId = surveyId;
-  }
+    public Survey(long surveyId, String surveyName, String description, String state, Timestamp createdAt, Timestamp updatedAt, long userId) {
+        this.surveyId = surveyId;
+        this.surveyName = surveyName;
+        this.description = description;
+        this.state = state;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.userId = userId;
+    }
 
-  public long getCourseId() {
-    return courseId;
-  }
+    public long getSurveyId() {
+        return surveyId;
+    }
 
-  public void setCourseId(long courseId) {
-    this.courseId = courseId;
-  }
+    public void setSurveyId(long surveyId) {
+        this.surveyId = surveyId;
+    }
 
-  public String getSurveyName() {
-    return surveyName;
-  }
+    public String getSurveyName() {
+        return surveyName;
+    }
 
-  public void setSurveyName(String surveyName) {
-    this.surveyName = surveyName;
-  }
+    public void setSurveyName(String surveyName) {
+        this.surveyName = surveyName;
+    }
 
-  public boolean getSurveyState() {
-    return surveyState;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public void setSurveyState(boolean surveyState) {
-    this.surveyState = surveyState;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Survey{" +
+                "surveyId=" + surveyId +
+                ", surveyName='" + surveyName + '\'' +
+                ", description='" + description + '\'' +
+                ", state='" + state + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", userId=" + userId +
+                '}';
+    }
 }
