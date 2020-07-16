efb3fdd18befbf20f3a7931a47073c7142aca496
Monil Panchal <mn204627@dal.ca>
2020-07-15 23:32:09 -0300

    [CHANGE] Using Builder pattern for creation Model survey, questions POJO objects

--- src/main/java/com/assessme/service/SurveyServiceImpl.java
+++ src/main/java/com/assessme/service/SurveyServiceImpl.java
@@ -132,37 +132,5 @@ public class SurveyServiceImpl implements SurveyService {
             throw e;
         }
     }
-//    @Override
-//    public Optional<List<Survey>> getSurveyList() throws Exception {
-//        Optional<List<Survey>> surveyList;
-//        try{
-//            surveyList = Optional.of(surveyDAOImpl.getAllSurvey());
-//
-//            String resMessage = String.format("Survey list has been retrieved from the database");
-//            logger.info(resMessage);
-//        }catch (Exception e){
-//            String errMessage = String.format("Error in retrieving the survey list from the database");
-//            logger.error(errMessage);
-//            e.printStackTrace();
-//            throw e;
-//        }
-//        return surveyList;
-//    }
 
-
-//    @Override
-//    public Optional<Survey> getSurveyByCourse(long courseId) throws Exception {
-//        Optional<Survey> newSurvey;
-//        try {
-//            newSurvey = surveyDAOImpl.getSurveyByCourse(courseId);
-//            String resMessage = String.format("Survey %s found for course id: %d ", newSurvey.get().getSurveyName(), courseId);
-//            logger.info(resMessage);
-//        } catch (Exception e) {
-//            String errMessage = String.format("No survey found for course id: %d in the database", courseId);
-//            logger.error(errMessage);
-//            e.printStackTrace();
-//            throw e;
-//        }
-//        return newSurvey;
-//    }
 }
