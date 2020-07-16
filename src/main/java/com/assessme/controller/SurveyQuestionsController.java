61b8f1810745dd257837eba1732b0f8790f4a267
HARDIK PATEL <hr325484@dal.ca>
2020-07-16 16:38:11 -0300

    Listing questions on student survey page

--- src/main/java/com/assessme/controller/SurveyQuestionsController.java
+++ src/main/java/com/assessme/controller/SurveyQuestionsController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -97,12 +98,37 @@ public class SurveyQuestionsController {
     }
 
     @GetMapping(value = "/get_questions_details")
+    public ModelAndView getQuestionsDetailsBySurveyId(@RequestParam("surveyId") Long surveyId) {
+
+        logger.info("Calling API for questions details retrieval for the survey: " + surveyId);
+        HttpStatus httpStatus = null;
+        ResponseDTO<List<Survey>> responseDTO = null;
+        ModelAndView modelAndView = new ModelAndView("student_survey");
+        try {
+            Optional<SurveyQuestionsDetails> surveyList = surveyQuestionsService.getSurveyQuestionsDetails(surveyId);
+            String resMessage = String.format("Survey question list has been retrieved from the database");
+            modelAndView.addObject("surveyQuestions",surveyList.get().getQuestions());
+            modelAndView.addObject("surveyId",surveyList.get().getSurveyId());
+            responseDTO = new ResponseDTO(true, resMessage, null, surveyList.get());
+            httpStatus = HttpStatus.OK;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+
+            String errMessage = String.format("Error in retrieving the survey questions from the database");
+            responseDTO = new ResponseDTO(false, errMessage, e.getLocalizedMessage(), null);
+            httpStatus = HttpStatus.CONFLICT;
+        }
+
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/api/get_questions_details")
     public ResponseEntity<ResponseDTO> getQuestionsDetailsForSurvey(@RequestParam("surveyId") Long surveyId) {
 
         logger.info("Calling API for questions details retrieval for the survey: " + surveyId);
         HttpStatus httpStatus = null;
         ResponseDTO<List<Survey>> responseDTO = null;
-
         try {
             Optional<SurveyQuestionsDetails> surveyList = surveyQuestionsService.getSurveyQuestionsDetails(surveyId);
             String resMessage = String.format("Survey question list has been retrieved from the database");
